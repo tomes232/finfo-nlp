53fc2c8818e1e315fb116eaaa5e120af1188e6ef
Ryan Baker <rjbx@umich.edu>
2021-10-25 18:50:51 -0400

    cleaned up and adjusted styles

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
 from q_and_a import genAnswer
 import random
 import json
 from flask import Flask, render_template, request
 from flask_ngrok import run_with_ngrok
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'top-secret!'
 
 @app.route("/")
@@ -17,13 +16,6 @@ def home():
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    print("like")
     incoming_msg = request.form["msg"]
-    print(incoming_msg)
-    print(type(incoming_msg))
     answer = genAnswer(incoming_msg, "file-QRZKYePIEhaQGkjS8ajlzPXJ")["answers"][0]
-    print("big Butts")
-    r = MessagingResponse()
-    r.message(answer)
-    print(str(r))
-    return str(r)
+    return str(answer)
