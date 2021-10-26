4a5bf8bf2297d75a86f6ced3fd678bc6e881db44
Ryan Baker <rjbx@umich.edu>
2021-10-26 13:58:18 -0400

    made some styling changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import random
 import json
 from flask import Flask, render_template, request
 from flask_ngrok import run_with_ngrok
+import openai
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'top-secret!'
@@ -17,5 +18,8 @@ def home():
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.form["msg"]
-    answer = genAnswer(incoming_msg, "file-QRZKYePIEhaQGkjS8ajlzPXJ")["answers"][0]
+    try:
+        answer = genAnswer(incoming_msg, "file-QRZKYePIEhaQGkjS8ajlzPXJ")["answers"][0]
+    except openai.error.InvalidRequestError:
+        answer = "I don't have enough information to answer that question. Please try another."
     return str(answer)
